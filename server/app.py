74ab6a6f754cdd3c8b9d51c9e3e3e776586e435d
MNdagu <ndagumark@gmail.com>
2024-10-09 20:43:26 +0300

    Final

--- server/app.py
+++ server/app.py
@@ -20,19 +20,32 @@ def index():
 
 @app.route('/bakeries')
 def bakeries():
-    return ''
+    bakeries = Bakery.query.all()
+    bakeries_list = [bakery.to_dict() for bakery in bakeries]
+    return jsonify(bakeries_list)
 
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-    return ''
+    bakery = Bakery.query.filter_by(id=id).first()
+    if bakery:
+        return jsonify(bakery.to_dict())
+    else:
+        return jsonify({"message": "No bakery found"}), 404
+    
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
+    baked_goods = BakedGood.query.order_by(BakedGood.price.desc()).all()
+    baked_goods_list = [baked_good.to_dict() for baked_good in baked_goods]
+    return jsonify(baked_goods_list)
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    return ''
+    most_expensive = BakedGood.query.order_by(BakedGood.price.desc()).limit(1).first()
+    if most_expensive:
+        return jsonify(most_expensive.to_dict())
+    else:
+        return jsonify({"message": "No baked goods found"}), 404
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
